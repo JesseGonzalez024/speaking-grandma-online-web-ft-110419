fb9e558bcdc225af288ff675e33fe2555e844c40
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-05 22:52:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@ def speak_to_grandma(string)
     return 'HUH?! SPEAK UP, SONNY!'
   elsif string == string.upcase
     return 'NO, NOT SINCE 1938!'
-  elsif string == 
+  elsif string
     return 'HUH?! SPEAK UP, SONNY!'
   else
   
