5f54ae9599960e297f1fe92f6584fd451a341cee
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-05 22:35:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,4 +13,14 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
+def speak_to_grandma(string)
+  if string == string.downcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif string == string.upcase
+    puts "O, NOT SINCE 1938!"
+  else string == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  end
+end
 
+speak_to_grandma("LOVE YOU GRANDMA!")
